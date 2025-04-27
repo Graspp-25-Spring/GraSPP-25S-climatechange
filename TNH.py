bc29da9c4aa85c9edf041a07d50f0335918dd95c
TNaingHtwe <kothetnaing96@gmail.com>
2025-04-28 00:53:57 +0900

    Update TNH.py
    Sorry,  no idea where to being with T_T

--- TNH.py
+++ TNH.py
@@ -1,5 +1,9 @@
+<<<<<<< Updated upstream
 def string_print(a):
     return a
 
 result = string_print('LOOK MOM I CAN PRINT')
 print(f"The result of string_print function: {result}")
+=======
+print("something")
+>>>>>>> Stashed changes
